1aa1ec3f3821945911ec3693480410a4e6851361
fulldump <>
2018-01-22 00:09:00 +0100

    List statics

--- src/blogo/statics/api.go
+++ src/blogo/statics/api.go
@@ -1,6 +1,8 @@
 package statics
 
 import (
+	"fmt"
+
 	"github.com/fulldump/golax"
 )
 
@@ -10,6 +12,12 @@ func Build(node *golax.Node, statics string) {
 	if "" != statics {
 		m = readFileExternal(statics)
 	}
+
+	node.Node("statics").Method("GET", func(c *golax.Context) {
+		for filename, _ := range Files {
+			fmt.Fprintln(c.Response, "<a href='/"+filename+"'>"+filename+"</a><br>")
+		}
+	})
 
 	node.Node("{{*}}").Method("GET", func(c *golax.Context) {
 		m(c, c.Parameter)
