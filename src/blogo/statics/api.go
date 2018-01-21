2c8767c187b759cf96331ef2be27e87e9321ac06
fulldump <>
2018-01-22 00:59:59 +0100

    Prueba 2222222

--- src/blogo/statics/api.go
+++ src/blogo/statics/api.go
@@ -9,18 +9,25 @@ import (
 func Build(node *golax.Node, statics string) {
 
 	m := readFileInternal
-	if "" != statics {
-		fmt.Println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
-		m = readFileExternal(statics)
-	}
+
+	/*
+		if "" != statics {
+			fmt.Println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
+			m = readFileExternal(statics)
+		}
+	*/
 
 	node.Node("statics").Method("GET", func(c *golax.Context) {
 		for filename, _ := range Files {
 			fmt.Fprintln(c.Response, "<a href='/"+filename+"'>"+filename+"</a><br>")
 		}
+		for filename, _ := range Bytes {
+			fmt.Fprintln(c.Response, "<a href='/"+filename+"'>"+filename+"</a><br>")
+		}
 	})
 
 	node.Node("{{*}}").Method("GET", func(c *golax.Context) {
+		fmt.Println(c.Parameter)
 		m(c, c.Parameter)
 	})
 
